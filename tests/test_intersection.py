5281944cfcbe1a0a04b08e2f8fd10bf9d9707871
ryan-foltz-exa <rfoltz@exabeam.com>
2018-10-31 16:46:59 -0700

    starting tests for intersection

--- tests/test_intersection.py
+++ tests/test_intersection.py
@@ -11,11 +11,45 @@ TODO: design test.
 import unittest
 from ghoul import Intersection
 
-class TestListIntersection(unittest.TestCase):
-    def test_list(self):
-        a = [1, 2, 3]
-        b = [1, 2, 3]
-        self.assertTrue(Intersection(a, b) == a)
+class TestLists(unittest.TestCase):
+    def test_ints(self):
+        # 1 ∩ 1 == 1
+        self.assertTrue(Intersection([1, 2, 3], [1, 2, 3]) == [1, 2, 3])
+        # 1 ∩ 2 == 2
+        self.assertTrue(Intersection([1, 2, 3], [1, 2]) == [1, 2])
+        # 1 ∩ 2 == 1
+        self.assertTrue(Intersection([1, 2], [1, 2, 3]) == [1, 2])
+        # 1 ∩ 2 < 1 and 1 ∩ 2 < 2
+        self.assertTrue(Intersection([1, 2, 3], [2, 3, 4]) == [2, 3])
+        # 1 ∩ 2 == ∅
+        self.assertIsNone(Intersection([1, 2, 3], [6, 7, 8]))
+        # len(1 ∩ 2) == 1
+        self.assertTrue(Intersection([1, 2, 3], [3]) == 3)
+    def test_str(self):
+        # 1 ∩ 1 == 1
+        self.assertTrue(Intersection(['a', 'b', 'c'], ['a', 'b', 'c']) == ['a', 'b', 'c'])
+        # 1 ∩ 2 == 2
+        self.assertTrue(Intersection(['a', 'b', 'c'], ['b', 'c']) == ['b', 'c'])
+        # 1 ∩ 2 == 1
+        self.assertTrue(Intersection(['a', 'b'], ['a', 'b', 'c']) == ['a', 'b'])
+        # 1 ∩ 2 < 1 and 1 ∩ 2 < 2
+        self.assertTrue(Intersection(['a', 'b', 'c'], ['b', 'c', 'd']) == ['b', 'c'])
+        # 1 ∩ 2 == ∅
+        self.assertIsNone(Intersection(['a', 'b', 'c'], ['d', 'e', 'f']))
+        # len(1 ∩ 2) == 1
+        self.assertTrue(Intersection(['a', 'b', 'c'], ['a']) == 'a')
+    # duplicates
+
+class TestValues(unittest.TestCase):
+    def test_ints(self):
+        self.assertTrue(Intersection(1, 1) == 1)
+        self.assertIsNone(Intersection(1, 2))
+    def test_str(self):
+        self.assertTrue(Intersection('a', 'a') == 'a')
+        self.assertIsNone(Intersection('a', 'b'))
+
+class TestAlgebraicProperties(unittest.TestCase):
+    pass
 
 if __name__ == '__main__':
   unittest.main()
