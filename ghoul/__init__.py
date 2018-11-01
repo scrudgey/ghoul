6f7179e5b4767ea8314f6751b4f34bf9fa51b8e0
ryan-foltz-exa <rfoltz@exabeam.com>
2018-11-01 16:27:22 -0700

    removed 'import *'

--- ghoul/__init__.py
+++ ghoul/__init__.py
@@ -6,5 +6,5 @@ __author__ = """Ryan Foltz"""
 __email__ = 'scrudgey@gmail.com'
 __version__ = '0.1.0'
 
-from ghoul.main import *
+from ghoul.main import Symbol, Intersection, symbol_includes, symbol_includes_all, symbol_includes_any
 
